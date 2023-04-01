953aab8590feae541b5596d8a9967048e75c804c
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-04-01 10:29:03 +0530

    update

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDataBase = ()=>{
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser: true, useUnifiedTopology: true}).then((data)=>{
+    mongoose.connect("mongodb://127.0.0.1:27017/pathmedDB",{useNewUrlParser: true, useUnifiedTopology: true}).then((data)=>{
         console.log(`Mongodb connected with server: ${data.connection.host}`);
     })
 }
