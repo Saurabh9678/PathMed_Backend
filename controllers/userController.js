b418005690e4a1b6122f5e4cae6712227c91dc3b
Saurabh <gohainsaurabh@gmail.com>
2023-04-01 10:28:14 +0530

    Register login logout user

---
+++ controllers/userController.js
@@ -0,0 +1,55 @@
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncError = require("../middleware/catchAsyncError");
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+
+// Register a User
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  sendToken(user, 201, res);
+});
+
+// Login User
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  sendToken(user, 200, res);
+});
+
+// Logout User
+exports.logoutUser = catchAsyncError(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
---
+++ routes/userRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const {
+  loginUser,
+  registerUser,
+  logoutUser,
+} = require("../controllers/userController");
+
+
+const router = express.Router()
+
+router.route("/register").post(registerUser)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").get(logoutUser)
+
+module.exports = router
